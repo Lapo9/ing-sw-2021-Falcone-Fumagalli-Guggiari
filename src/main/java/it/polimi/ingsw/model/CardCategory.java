798e4b5fab6edb799674742a4328834159df7d88
IlFumaa <marcofumagalli2910@gmail.com>
2021-03-22 16:46:56 +0100

    added all enums

---
+++ src/main/java/it/polimi/ingsw/model/CardCategory.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.model;
+
+public enum CardCategory {
+
+    GOLD1, GREY1, VIOLET1, BLUE1, GOLD2, GREY2, VIOLET2, BLUE2, GOLD3, GREY3, VIOLET3, BLUE3
+
+}
---
+++ src/main/java/it/polimi/ingsw/model/MarbleColor.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.model;
+
+public enum MarbleColor {
+
+    GOLD, GREY, VIOLET, BLUE
+
+}
