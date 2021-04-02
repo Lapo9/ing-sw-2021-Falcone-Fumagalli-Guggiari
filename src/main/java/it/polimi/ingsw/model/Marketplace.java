0546d2cf210dde400d2945cc7fb0a177772c0ff2
IlFumaa <marcofumagalli2910@gmail.com>
2021-04-02 10:52:37 +0200

    Marketplace class: added obtain method

--- src/main/java/it/polimi/ingsw/model/Marketplace.java
+++ src/main/java/it/polimi/ingsw/model/Marketplace.java
@@ -35,18 +35,18 @@ public class Marketplace implements HasStatus{
         }
         listOfMarbles.add(MarbleColor.RED);
 
-        //cycle through the positions of the grid, adn add a random marble. Then remove the added marble from the listOfMarbles
+        //cycle through the positions of the grid, and add a random marble. Then remove the added marble from the listOfMarbles
         for (int i=0; i<12; ++i){
-            int rnd = (int)(Math.random() * (13-i));
-            MarbleColor tmp = listOfMarbles.remove(rnd);
-            grid.add(tmp);
+            int rnd = (int)(Math.random() * (13-i)); //creating random number
+            MarbleColor tmp = listOfMarbles.remove(rnd); //remove casually one marble from the list
+            grid.add(tmp); //insert the removed marble in the grid
         }
-        slide = listOfMarbles.get(0);
+        slide = listOfMarbles.get(0); //insert the remaining marble in the slide
 
     }
 
 
-    //returns the index given row and column
+    //returns the index of the array list given row and column
     private int getPos(int r, int c){
         return c+r*4;
     }
@@ -64,17 +64,72 @@ public class Marketplace implements HasStatus{
      */
     public MarbleContainer obtain(MarketDirection dir, int index){
 
-        MarbleContainer mc;
+        int y=0, b=0, g=0, w=0, v=0, r=0;
 
-        if (dir == dir.HORIZONTAL){
+        //if the direction is HORIZONTAL, i have to take the marbles consequently with a cycle for
+        if (dir == MarketDirection.HORIZONTAL){
 
+            for(int i = index * 4; i < (index * 4) + 3; i++){
+                MarbleColor currentColor = grid.get(i); // get one element from the array list grid
 
-
+                //increment the counter for the got marble
+                if(currentColor == MarbleColor.YELLOW)
+                    y++;
+                else if(currentColor == MarbleColor.BLUE)
+                    b++;
+                else if(currentColor == MarbleColor.GREY)
+                    g++;
+                else if(currentColor == MarbleColor.WHITE)
+                    w++;
+                else if(currentColor == MarbleColor.VIOLET)
+                    v++;
+                else
+                    r++;
+            }
         }
-        else {
 
+        //if the direction is VERTICAL, i have to take one marble and skip the next three
+        else{
+            for(int i = index; i < 12; i++){
+
+                MarbleColor currentColor1 = grid.get(i); // get one element from the array list grid
+
+                //increment the counter for the got marble
+                if(currentColor == MarbleColor.YELLOW)
+                    y++;
+                else if(currentColor == MarbleColor.BLUE)
+                    b++;
+                else if(currentColor == MarbleColor.GREY)
+                    g++;
+                else if(currentColor == MarbleColor.WHITE)
+                    w++;
+                else if(currentColor == MarbleColor.VIOLET)
+                    v++;
+                else
+                    r++;
+                i += 3; //so i can take one element and skip the next three
+            }
         }
 
+        MarbleContainer mc = new MarbleContainer(y, b, g, w, v, r);
+
+        shiftMarket();
+
+        return mc;
+    }
+
+    /**
+     * This private method allows to shift the market after one player get his marbles
+     */
+    private void shiftMarket(){
+
+    }
+
+
+    //TODO
+    @Override
+    public ArrayList<Integer> getStatus(){
+        return new ArrayList<>();
     }
 
 }
