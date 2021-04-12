d4c9f0f88e9a2e57fb039d46888a58562c8b1d7e
IlFumaa <marcofumagalli2910@gmail.com>
2021-04-12 23:08:27 +0200

    LeaderCardTest added

---
+++ src/test/java/it/polimi/ingsw/model/LeaderCardTest.java
@@ -0,0 +1,83 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.exceptions.LeaderException;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class LeaderCardTest {
+
+    @Test
+    public void activate_no_exc() {
+        SupplyContainer reqSupplyContainer = new SupplyContainer(2, 0, 0, 0, 0);
+        ArrayList<CardsRequirement> reqDevelopmentCard = new ArrayList<>();
+        CardsRequirement cs1 = new CardsRequirement(1, 1, CardCategory.YELLOW);
+        CardsRequirement cs2 = new CardsRequirement(1, 2, CardCategory.BLUE);
+        reqDevelopmentCard.add(cs1);
+        reqDevelopmentCard.add(cs2);
+        LeaderAbility la = new LeaderAbility() {
+            @Override
+            public SupplyContainer getDiscount() throws NoSuchMethodException {
+                return null;
+            }
+        };
+        LeaderCard lc = new LeaderCard(reqSupplyContainer, reqDevelopmentCard, la, 5);
+        boolean exc = false;
+        try {
+            lc.activate();
+        } catch (LeaderException e) {
+            exc = true;
+        }
+
+        assertFalse(exc);
+    }
+
+    @Test
+    public void discard_no_exc() {
+        SupplyContainer reqSupplyContainer = new SupplyContainer(2, 0, 0, 0, 0);
+        ArrayList<CardsRequirement> reqDevelopmentCard = new ArrayList<>();
+        CardsRequirement cs1 = new CardsRequirement(1, 1, CardCategory.YELLOW);
+        CardsRequirement cs2 = new CardsRequirement(1, 2, CardCategory.BLUE);
+        reqDevelopmentCard.add(cs1);
+        reqDevelopmentCard.add(cs2);
+        LeaderAbility la = new LeaderAbility() {
+            @Override
+            public SupplyContainer getDiscount() throws NoSuchMethodException {
+                return null;
+            }
+        };
+        LeaderCard lc = new LeaderCard(reqSupplyContainer, reqDevelopmentCard, la, 5);
+        boolean exc = false;
+        try {
+            lc.discard();
+        } catch (LeaderException e) {
+            exc = true;
+        }
+
+        assertFalse(exc);
+    }
+
+    @Test
+    public void getWinPointsTest() {
+        SupplyContainer reqSupplyContainer = new SupplyContainer(2, 0, 0, 0, 0);
+        ArrayList<CardsRequirement> reqDevelopmentCard = new ArrayList<>();
+        CardsRequirement cs1 = new CardsRequirement(1, 1, CardCategory.YELLOW);
+        CardsRequirement cs2 = new CardsRequirement(1, 2, CardCategory.BLUE);
+        reqDevelopmentCard.add(cs1);
+        reqDevelopmentCard.add(cs2);
+        LeaderAbility la = new LeaderAbility() {
+            @Override
+            public SupplyContainer getDiscount() throws NoSuchMethodException {
+                return null;
+            }
+        };
+        LeaderCard lc = new LeaderCard(reqSupplyContainer, reqDevelopmentCard, la, 5);
+
+        int expectedWP = 5;
+        int actualWP = lc.getWinPoints();
+
+        assertEquals(expectedWP, actualWP);
+    }
+}
