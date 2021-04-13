4c0039af19e5680fc876011f9ece74884d179261
sofiaguggiari <sofia.guggiari@mail.polimi.it>
2021-04-13 09:46:19 +0200

    Add some tests in ProductionManager test class (not ended yet)

--- src/test/java/it/polimi/ingsw/model/ProductionManagerTest.java
+++ src/test/java/it/polimi/ingsw/model/ProductionManagerTest.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.Pair;
 import it.polimi.ingsw.exceptions.SupplyException;
 import org.junit.Test;
 
@@ -8,11 +9,84 @@ import static org.junit.Assert.*;
 public class ProductionManagerTest {
 
     @Test
-    public void addSupply() {
+    public void addSupply_development() {
+        ProductionManager prdmng = new ProductionManager(new Developments(), new MutableProduction(2, 1), new LeadersSpace());
+        boolean exc = false;
+        try {
+            prdmng.addSupply(DepotID.DEVELOPMENT3, WarehouseObjectType.STONE, DepotID.WAREHOUSE3);
+        } catch (SupplyException e) {
+            exc = true;
+        }
+        assertFalse(exc);
     }
 
     @Test
-    public void removeSupply() {
+    public void addSupply_production() {
+        ProductionManager prdmng = new ProductionManager(new Developments(), new MutableProduction(2, 1), new LeadersSpace());
+        boolean exc = false;
+        try {
+            prdmng.addSupply(DepotID.BASE_PRODUCTION, WarehouseObjectType.STONE, DepotID.WAREHOUSE2);
+        } catch (SupplyException e) {
+            exc = true;
+        }
+        assertFalse(exc);
+    }
+
+    @Test
+    public void addSupply_leader() {
+        ProductionManager prdmng = new ProductionManager(new Developments(), new MutableProduction(2, 1), new LeadersSpace());
+        boolean exc = false;
+        try {
+            prdmng.addSupply(DepotID.LEADER1_PRODUCTION, WarehouseObjectType.COIN, DepotID.LEADER1_DEPOT);
+        } catch (SupplyException e) {
+            exc = true;
+        }
+        assertFalse(exc);
+    }
+
+    @Test
+    public void removeSupply_development() {
+        ProductionManager prdmng = new ProductionManager(new Developments(), new MutableProduction(2, 1), new LeadersSpace());
+        boolean exc = false;
+        try {
+            prdmng.addSupply(DepotID.DEVELOPMENT3, WarehouseObjectType.STONE, DepotID.WAREHOUSE3);
+        } catch (SupplyException e) {fail();}
+        try {
+            prdmng.removeSupply(DepotID.DEVELOPMENT3, WarehouseObjectType.STONE, DepotID.WAREHOUSE3);
+        } catch (SupplyException e) {
+            exc = true;
+        }
+        assertFalse(exc);
+    }
+
+    @Test
+    public void removeSupply_production() {
+        ProductionManager prdmng = new ProductionManager(new Developments(), new MutableProduction(2, 1), new LeadersSpace());
+        boolean exc = false;
+        try {
+            prdmng.addSupply(DepotID.BASE_PRODUCTION, WarehouseObjectType.COIN, DepotID.LEADER1_DEPOT);
+        } catch (SupplyException e) {fail();}
+        try {
+            prdmng.removeSupply(DepotID.BASE_PRODUCTION, WarehouseObjectType.COIN, DepotID.LEADER1_DEPOT);
+        } catch (SupplyException e) {
+            exc = true;
+        }
+        assertFalse(exc);
+    }
+
+    @Test
+    public void removeSupply_leader() {
+        ProductionManager prdmng = new ProductionManager(new Developments(), new MutableProduction(2, 1), new LeadersSpace());
+        boolean exc = false;
+        try {
+            prdmng.addSupply(DepotID.LEADER2_PRODUCTION, WarehouseObjectType.SHIELD, DepotID.WAREHOUSE2);
+        } catch (SupplyException e) {fail();}
+        try {
+            prdmng.removeSupply(DepotID.LEADER2_PRODUCTION, WarehouseObjectType.SHIELD, DepotID.WAREHOUSE2);
+        } catch (SupplyException e) {
+            exc = true;
+        }
+        assertFalse(exc);
     }
 
     @Test
@@ -75,7 +149,90 @@ public class ProductionManagerTest {
     }
 
     @Test
-    public void clearSupplies() {
+    public void clearSupplies_fromDevelopment() {
+        ProductionManager prdmng = new ProductionManager(new Developments(), new MutableProduction(2, 1), new LeadersSpace());
+        try {
+            prdmng.addSupply(DepotID.DEVELOPMENT3, WarehouseObjectType.COIN, DepotID.WAREHOUSE2);
+            prdmng.addSupply(DepotID.DEVELOPMENT3, WarehouseObjectType.STONE, DepotID.WAREHOUSE3);
+        } catch (SupplyException e) {fail();}
+        Pair<SupplyContainer, SupplyContainer> result = null;
+        try {
+            result = new Pair<>(prdmng.clearSupplies(DepotID.DEVELOPMENT3));
+        } catch (NoSuchMethodException e) {fail();}
+        SupplyContainer totalResources = new SupplyContainer(result.first.sum(result.second));
+        int[] expectedResult = {1, 1, 0, 0, 0};
+        int[] actualResult = {totalResources.getQuantity(WarehouseObjectType.COIN),
+                              totalResources.getQuantity(WarehouseObjectType.STONE),
+                              totalResources.getQuantity(WarehouseObjectType.SHIELD),
+                              totalResources.getQuantity(WarehouseObjectType.SERVANT),
+                              totalResources.getQuantity(WarehouseObjectType.FAITH_MARKER)};
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void clearSupplies_fromProduction() {
+        ProductionManager prdmng = new ProductionManager(new Developments(), new MutableProduction(2, 1), new LeadersSpace());
+        try {
+            prdmng.addSupply(DepotID.BASE_PRODUCTION, WarehouseObjectType.SHIELD, DepotID.WAREHOUSE2);
+            prdmng.addSupply(DepotID.BASE_PRODUCTION, WarehouseObjectType.SHIELD, DepotID.WAREHOUSE3);
+        } catch (SupplyException e) {fail();}
+        Pair<SupplyContainer, SupplyContainer> result = null;
+        try {
+            result = new Pair<>(prdmng.clearSupplies(DepotID.BASE_PRODUCTION));
+        } catch (NoSuchMethodException e) {fail();}
+        SupplyContainer totalResources = new SupplyContainer(result.first.sum(result.second));
+        int[] expectedResult = {0, 0, 2, 0, 0};
+        int[] actualResult = {totalResources.getQuantity(WarehouseObjectType.COIN),
+                              totalResources.getQuantity(WarehouseObjectType.STONE),
+                              totalResources.getQuantity(WarehouseObjectType.SHIELD),
+                              totalResources.getQuantity(WarehouseObjectType.SERVANT),
+                              totalResources.getQuantity(WarehouseObjectType.FAITH_MARKER)};
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void clearSupplies_fromLeader() {
+        ProductionManager prdmng = new ProductionManager(new Developments(), new MutableProduction(2, 1), new LeadersSpace());
+        try {
+            prdmng.addSupply(DepotID.LEADER1_PRODUCTION, WarehouseObjectType.SERVANT, DepotID.WAREHOUSE1);
+            prdmng.addSupply(DepotID.LEADER1_PRODUCTION, WarehouseObjectType.SHIELD, DepotID.COFFER);
+        } catch (SupplyException e) {fail();}
+        Pair<SupplyContainer, SupplyContainer> result = null;
+        try {
+            result = new Pair<>(prdmng.clearSupplies(DepotID.BASE_PRODUCTION));
+        } catch (NoSuchMethodException e) {fail();}
+        SupplyContainer totalResources = new SupplyContainer(result.first.sum(result.second));
+        int[] expectedResult = {0, 0, 1, 1, 0};
+        int[] actualResult = {totalResources.getQuantity(WarehouseObjectType.COIN),
+                totalResources.getQuantity(WarehouseObjectType.STONE),
+                totalResources.getQuantity(WarehouseObjectType.SHIELD),
+                totalResources.getQuantity(WarehouseObjectType.SERVANT),
+                totalResources.getQuantity(WarehouseObjectType.FAITH_MARKER)};
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void clearSupplies_fromAll() {
+        ProductionManager prdmng = new ProductionManager(new Developments(), new MutableProduction(2, 1), new LeadersSpace());
+        try {
+            prdmng.addSupply(DepotID.DEVELOPMENT2, WarehouseObjectType.COIN, DepotID.WAREHOUSE1);
+            prdmng.addSupply(DepotID.DEVELOPMENT2, WarehouseObjectType.STONE, DepotID.COFFER);
+            prdmng.addSupply(DepotID.BASE_PRODUCTION, WarehouseObjectType.SHIELD, DepotID.WAREHOUSE2);
+            prdmng.addSupply(DepotID.BASE_PRODUCTION, WarehouseObjectType.SHIELD, DepotID.WAREHOUSE3);
+            prdmng.addSupply(DepotID.LEADER1_PRODUCTION, WarehouseObjectType.SERVANT, DepotID.COFFER);
+        } catch (SupplyException e) {fail();}
+        Pair<SupplyContainer, SupplyContainer> result = null;
+        try {
+            result = new Pair<>(prdmng.clearSupplies(DepotID.BASE_PRODUCTION));
+        } catch (NoSuchMethodException e) {fail();}
+        SupplyContainer totalResources = new SupplyContainer(result.first.sum(result.second));
+        int[] expectedResult = {1, 1, 2, 1, 0};
+        int[] actualResult = {totalResources.getQuantity(WarehouseObjectType.COIN),
+                              totalResources.getQuantity(WarehouseObjectType.STONE),
+                              totalResources.getQuantity(WarehouseObjectType.SHIELD),
+                              totalResources.getQuantity(WarehouseObjectType.SERVANT),
+                              totalResources.getQuantity(WarehouseObjectType.FAITH_MARKER)};
+        assertArrayEquals(expectedResult, actualResult);
     }
 
     @Test
