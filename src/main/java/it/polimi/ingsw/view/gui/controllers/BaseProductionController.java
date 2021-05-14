9aba4da177638ffe5f22d4a1de3552f4b1aa30a3
Lapo9 <lapofalcone@gmail.com>
2021-05-14 18:38:13 +0200

    Added BaseProductionController class

---
+++ src/main/java/it/polimi/ingsw/view/gui/controllers/BaseProductionController.java
@@ -0,0 +1,66 @@
+import it.polimi.ingsw.view.gui.controllers.SubSceneController;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class BaseProductionController extends SubSceneController {
+
+    @FXML private ImageView out;
+    @FXML private ImageView in1;
+    @FXML private ImageView in2;
+    @FXML private ImageView outUp;
+    @FXML private ImageView outDown;
+    @FXML private ImageView in2up;
+    @FXML private ImageView in1up;
+    @FXML private ImageView in2down;
+    @FXML private ImageView in1down;
+    @FXML private ImageView checkBoc;
+    @FXML private ImageView produce;
+
+
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
+
+    @Override
+    public void update(int[] completeUpdate) {
+
+    }
+
+    @FXML
+    void checkBoxClicked() {
+
+    }
+
+    @FXML
+    void in1downClicked() {
+
+    }
+
+    @FXML
+    void in1upClicked() {
+
+    }
+
+    @FXML
+    void in2downClicked() {
+
+    }
+
+    @FXML
+    void in2upClicked() {
+
+    }
+
+    @FXML
+    void outDownClicked() {
+
+    }
+
+    @FXML
+    void outUpClicked() {
+
+    }
+
+}
+
---
+++ src/main/java/it/polimi/ingsw/view/gui/controllers/DashboardController.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.view.gui.controllers;
+
+import javafx.fxml.FXML;
+
+public class DashboardController {
+
+    @FXML private LeaderController leader1Controller;
+    @FXML private LeaderController leader2Controller;
+    @FXML private WarehoseController warehouseController;
+    @FXML private CofferController cofferController;
+    @FXML private BaseProductionController baseProductionController;
+    @FXML private DevelopmentSpaceController developmentSpace1Controller;
+    @FXML private DevelopmentSpaceController developmentSpace2Controller;
+    @FXML private DevelopmentSpaceController developmentSpace3Controller;
+    @FXML private PaycheckController paycheckController;
+    @FXML private PopeFavorTilesController popeFavorTiles1Controller;
+    @FXML private PopeFavorTilesController popeFavorTiles2Controller;
+    @FXML private PopeFavorTilesController popeFavorTiles3Controller;
+    @FXML private PlayersController faithTrackPlayersController;
+
+
+}
