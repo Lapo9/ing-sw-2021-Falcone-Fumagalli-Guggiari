2c8308dcd20bc20fb93fd352db0f0e5d7a5dbe48
IlFumaa <marcofumagalli2910@gmail.com>
2021-06-15 12:30:55 +0200

    Commenting CLI

--- src/main/java/it/polimi/ingsw/view/cli/viewables/LeaderCardSpace.java
+++ src/main/java/it/polimi/ingsw/view/cli/viewables/LeaderCardSpace.java
@@ -5,6 +5,9 @@ import it.polimi.ingsw.view.cli.Viewable;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Represents the space containing the two LeaderCard(s) of the player
+ */
 public class LeaderCardSpace implements Viewable {
 
     private ArrayList<LeaderCard> leaderSpace = new ArrayList<>();
@@ -12,12 +15,21 @@ public class LeaderCardSpace implements Viewable {
     private int state1;
     private int state2;
 
+    /**
+     * Class constructor
+     * @param type of the LeaaderCard (Depot, Producer...)
+     */
     LeaderCardSpace(boolean type) {
         this.type = type;
         leaderSpace.add(new LeaderCard());
         leaderSpace.add(new LeaderCard());
     }
 
+    /**
+     * Updates the viewable using numbers from the getStatus
+     * @param update array composed of 30 int: (id, state, fixed input, fixed output, fixed output, 5 int for the current supply of the ability
+     *               Production, 5 int for the ability Depot if existing)x2
+     */
     @Override
     public void update(int[] update) {
         int[] update1 = Arrays.copyOfRange(update, 0, 15);
@@ -28,11 +40,19 @@ public class LeaderCardSpace implements Viewable {
         state2 = update2[1];
     }
 
+    /**
+     * Prints the two LeaderCard(s) of the player
+     * @return string with the LeaderCardSpace
+     */
     @Override
     public String toString() {
        return buildLeaderSpace();
     }
 
+    /**
+     * Builds the LeaderCardSpace
+     * @return the String to print
+     */
     private String buildLeaderSpace() {
         String tmp = "";
         if(type || (state2 != 0 && state1 != 0)) {
@@ -96,6 +116,12 @@ public class LeaderCardSpace implements Viewable {
         return tmp;
     }
 
+    /**
+     * Allows to print the two cards one beside the other
+     * @param leaderNumber number of the LeaderCard, we will return the line number "line" of this card
+     * @param line of the LeaderCard to print
+     * @return the line chosen of the LeaderCard chosen
+     */
     private String cutIntoRows(int leaderNumber, int line) {
         String[] rows = leaderSpace.get(leaderNumber).toString().split("\n");
         return rows[line];
--- src/main/java/it/polimi/ingsw/view/cli/viewables/LeaderPick.java
+++ src/main/java/it/polimi/ingsw/view/cli/viewables/LeaderPick.java
@@ -5,9 +5,16 @@ import it.polimi.ingsw.view.cli.Viewable;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Represents the space containing the four LeaderCard(s) picked by the player
+ * he will choose two of them
+ */
 public class LeaderPick implements Viewable {
     ArrayList<LeaderCard> leaderSpace = new ArrayList<>();
 
+    /**
+     * Class constructor
+     */
     LeaderPick() {
         leaderSpace.add(new LeaderCard());
         leaderSpace.add(new LeaderCard());
@@ -15,9 +22,14 @@ public class LeaderPick implements Viewable {
         leaderSpace.add(new LeaderCard());
     }
 
+    /**
+     * Updates the viewable using numbers from the getStatus
+     * @param update array composed of 60 int: (id, state, fixed input, fixed output, fixed output, 5 int for the current supply of the ability
+     *               Production, 5 int for the ability Depot if existing)x4
+     */
     @Override
     public void update(int[] update) {
-        int[] update1 = Arrays.copyOfRange(update, 0, 15);
+        int[] update1 = Arrays.copyOfRange(update, 0, 15); //calls the update of LeaderCardSpace
         leaderSpace.get(0).update(update1);
         int[] update2 = Arrays.copyOfRange(update, 15, 30);
         leaderSpace.get(1).update(update2);
@@ -27,11 +39,19 @@ public class LeaderPick implements Viewable {
         leaderSpace.get(3).update(update4);
     }
 
+    /**
+     * Prints the four LeaderCard(s) from which the player will have to choose
+     * @return string with the LeaderPick
+     */
     @Override
     public String toString() {
         return buildLeaderSpace();
     }
 
+    /**
+     * Builds the LeaderPick
+     * @return the String to print
+     */
     private String buildLeaderSpace() {
         String tmp = "";
 
@@ -51,6 +71,13 @@ public class LeaderPick implements Viewable {
         return tmp;
     }
 
+
+    /**
+     * Allows to print the two cards one beside the other
+     * @param leaderNumber number of the LeaderCard, we will return the line number "line" of this card
+     * @param line of the LeaderCard to print
+     * @return the line chosen of the LeaderCard chosen
+     */
     private String cutIntoRows(int leaderNumber, int line) {
         String[] rows = leaderSpace.get(leaderNumber).toString().split("\n");
         return rows[line];
